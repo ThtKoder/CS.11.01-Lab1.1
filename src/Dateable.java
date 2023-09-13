82991110b952cbf92142b41e1704a53daff526d8
sander <sanderd2026@student.cis.edu.hk>
2023-09-13 09:55:28 +0800

    Lab 1.1 Submission

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        System.out.println("Welcome to the minimum age you can date calculator. Please enter your age below," + "\n" + "followed by the return/enter key:" + "\n");
+        Scanner myObj = new Scanner(System.in);
+
+        int userAge = myObj.nextInt();
+        double userAgeDouble = userAge;
+        double userdateableAge = userAgeDouble/2 + 7;
+        System.out.println("\n" + userAge + "-year olds should date somebody who is at least " + userdateableAge + " years old" + "\n\nCredit: Thanks to Jim Cohoon at The University of Virginia");
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+            System.out.println("Welcome to the least number of pages you should read before you give up on a book calculator. Please enter your age below," + "\n" + "followed by the return/enter key:" + "\n");
+            Scanner myObj = new Scanner(System.in);
 
+            int userAge = myObj.nextInt();
+            int userPagesRead = 100 - userAge;
+            System.out.println("\n" + userAge + "-year olds should read at least " + userPagesRead + " pages before giving up on a book." + "\n\nCredit: Thanks to Jim Cohoon at The University of Virginia");
     }
 }
